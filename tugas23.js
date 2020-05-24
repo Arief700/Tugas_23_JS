fb8fd6cba8d23a200d7646109b697cec333ae5cc
Arief Setiawan <59144049+Arief700@users.noreply.github.com>
2020-05-25 03:00:37 +0700

    Update tugas23.js

--- tugas23.js
+++ tugas23.js
@@ -1,6 +1,6 @@
-var arr = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66]
+var arr = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
 
 var result = arr.filter(arr => {
-  return arr > 15
+  return arr > 15;
 })
 console.log(result);
